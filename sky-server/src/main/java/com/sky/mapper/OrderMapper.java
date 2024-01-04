097d48aada3e60af99f5d9a4033d80a79972f8bd
weipch <weipch5152@aliyun.com>
2024-01-04 15:15:42 +0800

    feat：订单状态定时传处理

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -3,8 +3,20 @@ package com.sky.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper
 public interface OrderMapper extends BaseMapper<Orders> {
+
+	/**
+	 * 根据状态和下单时间查询订单
+	 * @param status
+	 * @param orderTime
+	 */
+	@Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+	List<Orders> getByStatusAndOrdertimeLT(Integer status, LocalDateTime orderTime);
 
 }
